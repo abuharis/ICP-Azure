5222343c7d2ac0e98465879836221e0fe8c0229c
abuharis <abuharis709@gmail.com>
2021-09-27 21:16:53 +0300

    initial commit n DevOps pipelines

---
+++ agent_pool.sh
@@ -0,0 +1,19 @@
+# Automated way to create an a Pool Agent to run Pipeline Jobs
+
+# Step 1: Download the agent to the machine to which to make as an agent
+
+# Step 2: Untar the and configure
+
+./config.sh --unattended \
+  --url \
+  --auth \
+  --token \
+  --pool \
+  --agent \
+  --work \
+  --acceptTeeEula
+  
+  # --acceptTeeEula - accept the Team Explorer Everywhere End User License Agreement (macOS and Linux only)
+  
+# For more:  ./config.sh --help 
+#Refer: https://docs.microsoft.com/en-us/azure/devops/pipelines/agents/v2-linux?view=azure-devops#check-prerequisites
