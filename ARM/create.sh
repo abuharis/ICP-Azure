5716f7f2e37aea2f5e5d962c17dfaf75e9422ada
Salih, Abuharis (Extern: Accenturea) <abuharis.salih@accenture.com>
2021-09-27 23:56:22 +0300

    + SScript added

---
+++ ARM/create.sh
@@ -0,0 +1,8 @@
+# Create a RG to logically group resources using
+
+rg = ICP-RG
+az group create --name $rg --location westus
+
+ # Deploy to a resource group from template
+az deployment group create --resource-group $rg --template-file storage.json
+
